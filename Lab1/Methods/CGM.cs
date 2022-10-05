56b7c400439dd06311d5bc912e60b1f90972b805
rk-02 <74454100+rk-02@users.noreply.github.com>
2022-10-05 21:36:38 +0500

    CGM Method

--- Lab1/Methods/CGM.cs
+++ Lab1/Methods/CGM.cs
@@ -6,29 +6,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab1.Methods{
+namespace Methods{
     internal class CGM{
         private Vector X;
         
-        private Vector getR(Matrix A, Vector B, Vector curX){
+        public Vector Answer {
+            get { return X; }
+        }
+        private Vector getR(Matrix A, Vector B, Vector curX) {
             return A * curX - B;
         }
 
-        private double getTau(Matrix A, Vector R){
+        private double getTau(Matrix A, Vector R) {
             return Vector.Dot(R, R) / Vector.Dot(A * R, R);
         }
 
-        private double getA(double tau1, double tau, Vector R1, Vector R, double a){
-            double res = 0;
-            res = 1 - tau1 / tau / a * Vector.Dot(R1, R1) / Vector.Dot(R, R);
+        private double getA(double tau1, double tau, Vector R1, Vector R, double a) {
+            double res = 1 - tau1 / tau / a * Vector.Dot(R1, R1) / Vector.Dot(R, R);
             return 1 / res;
         }
-        public CGM(Matrix A, Vector B, Vector X, Vector startVector, double eps){
-            Vector a = new Vector(B.Length);
-            a.Fill(new double[4] {1, 1, 1, 1});
-            Vector R = getR(A, B, X);
+        public CGM(Matrix A, Vector B, Vector X, Vector startVector, double eps) {
+            double a = 1;
+            Vector R = getR(A, B, startVector);
+            Vector RPrev;
+            Vector curX = (Vector)startVector.Clone();
+            Vector prevX = (Vector)startVector.Clone();
+            Vector newX = new Vector(X.Length);
             double tau = getTau(A, R);
+            double tauPrev;
 
+            curX = a * curX - tau * a * R;
+            tauPrev = tau;
+            RPrev = (Vector)R.Clone();
+
+            while ((X - curX).EnergyNorm(A) >= eps) {
+                R = getR(A, B, curX);
+                tau = getTau(A, R);
+                a = getA(tau, tauPrev, R, RPrev, a);
+
+                newX = a * curX + (1 - a) * prevX - tau * a * R;
+
+                tauPrev = tau;
+                RPrev = R;
+
+                prevX = (Vector)curX.Clone();
+                curX = (Vector)newX.Clone();
+
+            }
+
+            this.X = (Vector)curX.Clone();
         }
     }
 }
