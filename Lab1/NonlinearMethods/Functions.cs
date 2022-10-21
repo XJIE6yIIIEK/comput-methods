2330b21e43658225091c5fa67aba67c10b2b194e
rk-02 <74454100+rk-02@users.noreply.github.com>
2022-10-21 08:59:14 +0500

    Init Commit

---
+++ Lab1/NonlinearMethods/Functions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Matrices;
+using Vectors;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonlinearMethods {
+    class Functions{
+        public double dF1dx(Vector X) {
+            return 1.0;
+        }
+
+        public double dF1dy(Vector X) {
+            return -Math.Sin(X[1] - 1);
+        }
+
+        public double dF2dx(Vector X)  {
+            return Math.Sin(X[0]);
+        }
+
+        public double dF2dy(Vector X) {
+            return 1.0;
+        }
+
+        public Vector F(Vector X) {
+            Vector v = new Vector(2);
+            v[0] = F1(X);
+            v[1] = F2(X);
+            return v;
+        }
+        public double F1(Vector X) {
+            return Math.Cos(X[1] - 1) + X[0] - 0.8;
+        }
+
+        public double F2(Vector X) {
+            return X[1] - Math.Cos(X[0]) - 2;
+        }
+
+        public Matrix derivF(Vector X) {
+            Matrix a = new Matrix(2, 2);
+            a[0, 0] = dF1dx(X);
+            a[0, 1] = dF1dy(X);
+            a[1, 0] = dF2dx(X);
+            a[1, 1] = dF2dy(X);
+            return a.Inverse;
+        }
+
+        public double Fi1(Vector X) {
+            return 0.8 - Math.Cos(X[1] - 1);
+        }
+
+        public double Fi2(Vector X)
+        {
+            return 2 + Math.Cos(X[0]);
+        }
+
+        public Vector Fi(Vector X){
+            Vector v = new Vector(2);
+            v[0] = Fi1(X);
+            v[1] = Fi2(X);
+            return v;
+        }
+    }
+}
---
+++ Lab1/NonlinearMethods/NM.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vectors;
+using Matrices;
+using Lab1;
+
+namespace NonlinearMethods {
+    class NM {
+        IOModule io;
+        Vector curVector;
+        Vector prevVector;
+        Vector startVector;
+        Functions f = new Functions();
+
+        public Vector Answer{
+            get { return curVector; }
+        }
+        public NM(Vector startVector, IOModule io) {
+            this.io = io;
+            this.prevVector = startVector;
+            this.startVector = startVector;
+            NewtonMethod();
+        } 
+        private void NewtonMethod() {
+            int iter = 0;
+            WriteHead();
+            do {
+                curVector = prevVector - f.derivF(prevVector) * f.F(prevVector);
+                double resNorm = f.F(curVector).Norm();
+                prevVector = curVector;
+
+                WriteStep(++iter, curVector, resNorm, f.F(curVector));
+            }
+            while(f.F(curVector).Norm() > 1E-12);
+        }
+
+        private void WriteStep(int iter, Vector X, double residualNorm, Vector F) {
+            string iterStr = string.Format("{0,5}", iter);
+            string x = io.PrettyfyDouble(X[0], 12);
+            string y = io.PrettyfyDouble(X[1], 12);
+            string resNorm = io.PrettyfyDouble(residualNorm, 12);
+            string F1 = io.PrettyfyDouble(F[0], 12);
+            string F2 = io.PrettyfyDouble(F[1], 12);
+
+            string str = $"| {iterStr} | {x} | {y} | {resNorm} | {F1} | {F2}";
+
+            io.WriteLine(str);
+        }
+
+        private void WriteHead() { 
+            string centeredIter = io.CenterString("Iter", 7);
+            string centeredX = io.CenterString("x", 14);
+            string centeredY = io.CenterString("y", 14);
+            string centeredResNorm = io.CenterString("Residual norm", 14);
+            string centeredF1 = io.CenterString("F1", 14);
+            string centeredF2 = io.CenterString("F2", 14);
+
+            string head = $"|{centeredIter}|{centeredX}|{centeredY}|{centeredResNorm}|{centeredF1}|{centeredF2}";
+
+            Console.WriteLine(head);
+        }
+
+    }
+}
