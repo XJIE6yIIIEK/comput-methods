0d1779fef2e6cc6559c0db99cf0f5430c31affb8
rk-02 <74454100+rk-02@users.noreply.github.com>
2022-10-25 12:45:47 +0500

    Create SIM.cs

---
+++ Lab1/NonlinearMethods/SIM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab1;
+using Vectors;
+
+namespace NonlinearMethods {
+    class SIM {
+        Vector NMsolution;
+        Vector startVector;
+        Vector curVector;
+        Vector prevVector;
+        Functions f = new Functions();
+        public SIM(Vector solution, Vector startVector) {
+            this.NMsolution = solution;
+            this.startVector = startVector;
+            prevVector = startVector;
+            SimpleIterationMethod();
+        }
+
+        public void SimpleIterationMethod() {
+            int itr = 1;
+            do {
+                curVector = f.Fi(prevVector);
+                prevVector = curVector;
+
+            }
+            while((f.F(curVector) - f.F(NMsolution)).Norm() >= 1E-4);
+        }
+
+        //Console.Write(itr++ + " ");
+        //Console.Write(curVector.ToString() + " ");
+        //Console.Write((f.F(curVector) - f.F(NMsolution)).Norm() + " ");
+        //Console.WriteLine((curVector - NMsolution).Norm());
+    }
+}
