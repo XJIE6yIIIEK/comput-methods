a04521ef7da399d7ead7b635dd61dfc4e25e9d7c
rk-02 <74454100+rk-02@users.noreply.github.com>
2022-10-25 12:44:58 +0500

    Update Functions.cs

--- Lab1/NonlinearMethods/Functions.cs
+++ Lab1/NonlinearMethods/Functions.cs
@@ -7,6 +7,70 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace NonlinearMethods {
+    interface _Functions {
+        double EvaluateDerivativeX(Vector X);
+
+        double EvaluateDerivativeY(Vector X);
+
+        double Evaluate(Vector X);
+    }
+
+    class F1 : _Functions {
+        public double EvaluateDerivativeX(Vector X) {
+            return 1.0;
+        }
+
+        public double EvaluateDerivativeY(Vector X) {
+            return -Math.Sin(X[1] - 1);
+        }
+
+        public double Evaluate(Vector X) {
+            return Math.Cos(X[1] - 1) + X[0] - 0.8;
+        }
+    }
+
+    class F2 : _Functions {
+        public double EvaluateDerivativeX(Vector X) {
+            return Math.Sin(X[0]);
+        }
+
+        public double EvaluateDerivativeY(Vector X) {
+            return 1.0;
+        }
+
+        public double Evaluate(Vector X) {
+            return X[1] - Math.Cos(X[0]) - 2;
+        }
+    }
+
+    class Fi1 : _Functions {
+        public double EvaluateDerivativeX(Vector X) {
+            return Math.Sin(X[1] - 1);
+        }
+
+        public double EvaluateDerivativeY(Vector X) {
+            return 1.0;
+        }
+
+        public double Evaluate(Vector X) {
+            return 0.8 - Math.Cos(X[1] - 1);
+        }
+    }
+
+    class Fi2 : _Functions {
+        public double EvaluateDerivativeX(Vector X) {
+            return Math.Sin(X[0]);
+        }
+
+        public double EvaluateDerivativeY(Vector X) {
+            return 1.0;
+        }
+
+        public double Evaluate(Vector X) {
+            return X[1] - Math.Cos(X[0]) - 2;
+        }
+    }
+
     class Functions{
         public double dF1dx(Vector X) {
             return 1.0;
